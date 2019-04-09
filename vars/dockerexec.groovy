218f7363d854606b7e955d97100faad7a5d11844
soumianisoumya <47141016+soumianisoumya@users.noreply.github.com>
2019-04-09 19:34:46 +0530

    Create dockerexec.groovy

---
+++ vars/dockerexec.groovy
@@ -0,0 +1,8 @@
+def call(String args){
+  echo "path=${args}"
+  sh "docker build -t jenkins-tomcat:${BUILD_NUMBER} ." 
+  sh "docker build -t jenkins-sql:${BUILD_NUMBER} . -f db.Dockerfile"
+  sh "docker tag jenkins-tomcat:${BUILD_NUMBER} jenkins-tomcat:latest" 
+  sh "docker tag jenkins-sql:${BUILD_NUMBER} jenkins-sql:latest"
+  sh "docker images"
+  }
