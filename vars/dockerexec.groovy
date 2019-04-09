cd61571c6742a26143abc74dd55dc297f489123c
soumianisoumya <47141016+soumianisoumya@users.noreply.github.com>
2019-04-09 19:41:45 +0530

    Update dockerexec.groovy

--- vars/dockerexec.groovy
+++ vars/dockerexec.groovy
@@ -1,6 +1,6 @@
 def call(String args){
   echo "path=${args}"
-  sh "docker build -t jenkins-tomcat:${BUILD_NUMBER} ." 
+  sh "docker build -t jenkins-tomcat:${BUILD_NUMBER} . -f Dockerfile" 
   sh "docker build -t jenkins-sql:${BUILD_NUMBER} . -f db.Dockerfile"
   sh "docker tag jenkins-tomcat:${BUILD_NUMBER} jenkins-tomcat:latest" 
   sh "docker tag jenkins-sql:${BUILD_NUMBER} jenkins-sql:latest"
