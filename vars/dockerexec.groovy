77a84a99d774f677b6ce623e6224b0b3aa39a3d7
soumianisoumya <47141016+soumianisoumya@users.noreply.github.com>
2019-04-09 19:27:42 +0530

    Delete dockerexec.groovy

--- vars/dockerexec.groovy
+++
@@ -1,9 +0,0 @@
-def call(String args){
-  echo "path=${args}"
-  sh "docker info"
-  sh "docker build -t jenkins-tomcat:${BUILD_NUMBER} ." 
-  sh "docker build -t jenkins-sql:${BUILD_NUMBER} . -f db.Dockerfile"
-  sh "docker tag jenkins-tomcat:${BUILD_NUMBER} jenkins-tomcat:latest" 
-  sh "docker tag jenkins-sql:${BUILD_NUMBER} jenkins-sql:latest"
-  sh "docker images"
-  }
