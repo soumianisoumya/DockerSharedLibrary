f2290d38b9022e611b5fc5085e349fc893ccc64d
soumianisoumya <47141016+soumianisoumya@users.noreply.github.com>
2019-04-10 21:58:32 +0530

    Update dockerexec.groovy

--- vars/dockerexec.groovy
+++ vars/dockerexec.groovy
@@ -1,5 +1,5 @@
 def call(String args){
   echo "path=${args}"
-  docker.build("soumianisoumya/dockersharedlibrary:${BUILD_NUMBER}") 
+  docker.build("soumianisoumya/sharedlibrary:${BUILD_NUMBER}") 
   sh "docker images"
   }
