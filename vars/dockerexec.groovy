2c0e3929466cf7e6a0aa6b9267dcad079a06da49
soumianisoumya <47141016+soumianisoumya@users.noreply.github.com>
2019-04-11 11:52:55 +0530

    Update dockerexec.groovy

--- vars/dockerexec.groovy
+++ vars/dockerexec.groovy
@@ -1,5 +1,5 @@
 def call(String args){
   echo "path=${args}"
-  app = docker.build("saumyaprashar/docker-demo:${BUILD_NUMBER}") 
+  dockerImage = docker.build("saumyaprashar/docker-demo:${BUILD_NUMBER}") 
   sh "docker images"
   }
